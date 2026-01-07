6248e50a49671f5d7e34a2944077d7b8b7a7aee9
M Martin <ophielia@yahoo.com>
2026-01-07 15:18:10 +0100

    LS-2169 - beginning tests

---
+++ listshop/src/androidUnitTest/kotlin/com/listshop/bff/client/usecases/SystemGetLaunchScreenUseCaseTest.kt
@@ -0,0 +1,245 @@
+package com.listshop.bff.client.usecases
+
+
+import com.listshop.analytics.Analytics
+import com.listshop.analytics.AnalyticsHandle
+import com.listshop.analytics.AppInfo
+import com.listshop.analytics.ClientType
+import com.listshop.analytics.initDummyAnalytics
+import com.listshop.bff.TestDatabaseHelper
+import com.listshop.bff.TestServiceLocator
+import com.listshop.bff.data.state.ConnectionStatus
+import com.listshop.bff.data.state.OnboardingViewState
+import com.listshop.bff.data.state.TransitionViewState
+import com.listshop.bff.test.server.TestDispatcherBuilder
+import com.listshop.bff.ucp.OnboardingUCP
+import junit.framework.TestCase.assertFalse
+import kotlinx.coroutines.runBlocking
+import okhttp3.mockwebserver.MockWebServer
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
+
+
+class SystemGetLaunchScreenUseCaseTest {
+
+    var useCaseProvider: OnboardingUCP? = null
+
+    val mockWebServer = MockWebServer()
+    var baseUrl: String = ""
+    var analyticsHandle: AnalyticsHandle? = null
+    var databaseTestHelper: TestDatabaseHelper? = null
+
+    @BeforeTest
+    fun setUp() {
+        mockWebServer.start()
+        baseUrl = mockWebServer.url("").toString()
+        baseUrl = baseUrl.substring(0, baseUrl.length.minus(1) )
+
+        val newAndBetterDispatcher = TestDispatcherBuilder("signIn")
+            .withConfigFile("loginSuccessConfig.json")
+            .withConfigFile("loginBadCredentialsConfig.json")
+            .withConfigFile("getAllShoppingListsConfig.json")
+            .build()
+
+        mockWebServer.dispatcher = newAndBetterDispatcher
+
+        if (analyticsHandle == null) {
+            val analytics = object : Analytics {
+                override fun sendEvent(eventName: String, eventArgs: Map<String, Any>) {
+                    println("eventName: ${eventName}, eventArgs: ${eventArgs.keys.joinToString(",") { key -> "[$key, ${eventArgs[key]}]" }}")
+                }
+            }
+            analyticsHandle = initDummyAnalytics(analytics)
+        }
+
+        initializeContext()
+
+
+    }
+    fun initializeContext() {
+        val appInfo = AppInfo(
+            baseUrl = baseUrl,
+            name = "name",
+            model = "model",
+            os = "os",
+            osVersion = "osVersion",
+            clientType = ClientType.IOS,
+            clientVersion = "1.1.0",
+            buildNumber = "buildNumber",
+            deviceId = "deviceId"
+        )
+        val locator: TestServiceLocator = TestServiceLocator(analyticsHandle!!, appInfo)
+        databaseTestHelper = locator.testDBHelper
+
+        useCaseProvider = locator.onboardingUCP
+    }
+
+    @AfterTest
+    fun shutDown() {
+        mockWebServer.shutdown()
+    }
+
+    @Test
+    fun `client incompatible - should receive error`(): Unit = runBlocking {
+        // start with clear database
+        databaseTestHelper?.clearDatabase()
+
+        val errorDispatcher = TestDispatcherBuilder("launchScreen")
+            .withConfigFile("launchScreenIncompatibleConfig.json")
+            .build()
+        mockWebServer.dispatcher = errorDispatcher
+
+        val connectionStatus = ConnectionStatus.Online
+
+        var result = useCaseProvider?.systemGetLaunchScreen(connectionStatus)
+        assertNotNull(result)
+        assertEquals("Current version 1.1.0 is not compatible", result._error?.message)
+
+        // restore original dispatcher
+    }
+
+    @Test
+    fun `launch with logged in user, offline - should go to local list`(): Unit = runBlocking {
+        // setup database / context
+        initializeContext()
+        val loggedInUser = databaseTestHelper?.standardUser()
+        databaseTestHelper?.setUser(loggedInUser)
+
+        val connectionStatus = ConnectionStatus.Online
+
+        var result = useCaseProvider?.legacySystemGetLaunchScreen(connectionStatus)
+        assertNotNull(result)
+        assertTrue(result.isSuccess)
+        assertTrue(result.value is TransitionViewState)
+        assertTrue(result.value is TransitionViewState.ListManagementScreen)
+
+        val shoppingLists = (result.value as TransitionViewState.ListManagementScreen).shoppingLists
+        assertNotNull(shoppingLists)
+
+    }
+
+    @Test
+    fun `launch with logged in user, online - should merge local list`(): Unit = runBlocking {
+        // setup database / context
+        initializeContext()
+        val loggedInUser = databaseTestHelper?.standardUser()
+        databaseTestHelper?.setUser(loggedInUser)
+
+        val connectionStatus = ConnectionStatus.Online
+
+        var result = useCaseProvider?.legacySystemGetLaunchScreen(connectionStatus)
+        assertNotNull(result)
+        assertTrue(result.isSuccess)
+        assertTrue(result.value is TransitionViewState)
+        assertTrue(result.value is TransitionViewState.ListManagementScreen)
+
+        val shoppingLists = (result.value as TransitionViewState.ListManagementScreen).shoppingLists
+        assertNotNull(shoppingLists)
+
+    }
+
+    @Test
+    fun `launch with logged in user, list deleted - go to most recent list`(): Unit = runBlocking {
+        // setup database / context
+        initializeContext()
+        val loggedInUser = databaseTestHelper?.standardUser()
+        databaseTestHelper?.setUser(loggedInUser)
+
+        val connectionStatus = ConnectionStatus.Online
+
+        var result = useCaseProvider?.legacySystemGetLaunchScreen(connectionStatus)
+        assertNotNull(result)
+        assertTrue(result.isSuccess)
+        assertTrue(result.value is TransitionViewState)
+        assertTrue(result.value is TransitionViewState.ListManagementScreen)
+
+        val shoppingLists = (result.value as TransitionViewState.ListManagementScreen).shoppingLists
+        assertNotNull(shoppingLists)
+
+    }
+
+    @Test
+    fun `launch with logged out user, go to onboarding`(): Unit = runBlocking {
+        // setup database / context
+        initializeContext()
+        val loggedInUser = databaseTestHelper?.standardUser()
+        databaseTestHelper?.setUser(loggedInUser)
+
+        val connectionStatus = ConnectionStatus.Online
+
+        var result = useCaseProvider?.legacySystemGetLaunchScreen(connectionStatus)
+        assertNotNull(result)
+        assertTrue(result.isSuccess)
+        assertTrue(result.value is TransitionViewState)
+        assertTrue(result.value is TransitionViewState.ListManagementScreen)
+
+        val shoppingLists = (result.value as TransitionViewState.ListManagementScreen).shoppingLists
+        assertNotNull(shoppingLists)
+
+    }
+
+    @Test
+    fun `launch with anonymous user, go to local list`(): Unit = runBlocking {
+        // setup database / context
+        initializeContext()
+        val loggedInUser = databaseTestHelper?.standardUser()
+        databaseTestHelper?.setUser(loggedInUser)
+
+        val connectionStatus = ConnectionStatus.Online
+
+        var result = useCaseProvider?.legacySystemGetLaunchScreen(connectionStatus)
+        assertNotNull(result)
+        assertTrue(result.isSuccess)
+        assertTrue(result.value is TransitionViewState)
+        assertTrue(result.value is TransitionViewState.ListManagementScreen)
+
+        val shoppingLists = (result.value as TransitionViewState.ListManagementScreen).shoppingLists
+        assertNotNull(shoppingLists)
+
+    }
+
+    @Test
+    fun `launch with anonymous user, first time user -  go to greeting`(): Unit = runBlocking {
+        // setup database / context
+        initializeContext()
+        val loggedInUser = databaseTestHelper?.standardUser()
+        databaseTestHelper?.setUser(loggedInUser)
+
+        val connectionStatus = ConnectionStatus.Online
+
+        var result = useCaseProvider?.legacySystemGetLaunchScreen(connectionStatus)
+        assertNotNull(result)
+        assertTrue(result.isSuccess)
+        assertTrue(result.value is TransitionViewState)
+        assertTrue(result.value is TransitionViewState.ListManagementScreen)
+
+        val shoppingLists = (result.value as TransitionViewState.ListManagementScreen).shoppingLists
+        assertNotNull(shoppingLists)
+
+    }
+
+    @Test
+    fun `launch with anonymous user, no list - go to greeting`(): Unit = runBlocking {
+        // setup database / context
+        initializeContext()
+        val loggedInUser = databaseTestHelper?.standardUser()
+        databaseTestHelper?.setUser(loggedInUser)
+
+        val connectionStatus = ConnectionStatus.Online
+
+        var result = useCaseProvider?.legacySystemGetLaunchScreen(connectionStatus)
+        assertNotNull(result)
+        assertTrue(result.isSuccess)
+        assertTrue(result.value is TransitionViewState)
+        assertTrue(result.value is TransitionViewState.ListManagementScreen)
+
+        val shoppingLists = (result.value as TransitionViewState.ListManagementScreen).shoppingLists
+        assertNotNull(shoppingLists)
+
+    }
+
+}
