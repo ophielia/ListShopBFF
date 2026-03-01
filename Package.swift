eaa42f8e8822b4a0a76d7bcb949ca6098c9ace88
Anka <runner@sat12-bq150-8438a553-7046-4921-8249-01572b08d4e0-4A27E3C21BCD.local>
2026-03-01 10:27:17 +0000

    KMP SPM package release for 0.1.9

--- Package.swift
+++ Package.swift
@@ -1,7 +1,11 @@
 // swift-tools-version:5.8
 import PackageDescription
 
+// BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
+let remoteKotlinUrl = "https://api.github.com/repos/ophielia/ListShopBFF/releases/assets/364590961.zip"
+let remoteKotlinChecksum = "819017ea1b299475ef63af955ff4bc2c61b5a83bd9fc1b6b7d029f1a29f15c7c"
 let packageName = "allshared"
+// END KMMBRIDGE BLOCK
 
 let package = Package(
     name: packageName,
@@ -17,7 +21,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: packageName,
-            path: "./allshared/build/XCFrameworks/debug/\(packageName).xcframework"
+            url: remoteKotlinUrl,
+            checksum: remoteKotlinChecksum
         )
         ,
     ]
